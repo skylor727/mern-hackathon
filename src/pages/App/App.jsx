adbc5f29f2c0855278fba879a1cd9f093ff471fa
Jim Clark <jim.clark@generalassemb.ly>
2022-03-03 11:35:23 -0800

    Finish useEffect

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,7 +5,6 @@ import { getUser } from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
@@ -13,10 +12,9 @@ export default function App() {
     <main className="App">
       { user ?
         <>
-          <NavBar user={user} setUser={setUser} />
           <Routes>
             {/* client-side route that renders the component instance if the path matches the url in the address bar */}
-            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders/new" element={<NewOrderPage user={user} setUser={setUser} />} />
             <Route path="/orders" element={<OrderHistoryPage />} />
           </Routes>
         </>
--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,13 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import * as itemsAPI from '../../utilities/items-api';
+import './NewOrderPage.css';
+import { Link } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+import MenuList from '../../components/MenuList/MenuList';
+import CategoryList from '../../components/CategoryList/CategoryList';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
 
-export default function NewOrderPage() {
+export default function NewOrderPage({ user, setUser }) {
   const [menuItems, setMenuItems] = useState([]);
+  const [activeCat, setActiveCat] = useState('');
+  const categoriesRef = useRef([]);
 
   useEffect(function() {
     async function getItems() {
       const items = await itemsAPI.getAll();
+      categoriesRef.current = items.reduce((cats, item) => {
+        const cat = item.category.name;
+        return cats.includes(cat) ? cats : [...cats, cat];
+      }, []);
       setMenuItems(items);
+      setActiveCat(categoriesRef.current[0]);
     }
     getItems();
   }, []);
@@ -16,9 +30,21 @@ export default function NewOrderPage() {
   // the FIRST render only
 
   return (
-    <>
-      <h1>NewOrderPage</h1>
-      <button onClick={() => setMenuItems(Date.now())}>Trigger re-render</button>
-    </>
+    <main className="NewOrderPage">
+      <aside>
+        <Logo />
+        <CategoryList
+          categories={categoriesRef.current}
+          activeCat={activeCat}
+          setActiveCat={setActiveCat}
+        />
+        <Link to="/orders" className="button btn-sm">PREVIOUS ORDERS</Link>
+        <UserLogOut user={user} setUser={setUser} />
+      </aside>
+      <MenuList
+        menuItems={menuItems.filter(item => item.category.name === activeCat)}
+      />
+      <OrderDetail />
+    </main>
   );
 }
