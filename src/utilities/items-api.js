9bdb11381429143fdeca4c1a09bd09903c5a4be1
Jim Clark <jim.clark@generalassemb.ly>
2022-03-03 13:03:01 -0800

    Starter code for Shopping Cart Part 1

--- src/utilities/items-api.js
+++ src/utilities/items-api.js
@@ -1,4 +1,4 @@
-import { getToken } from './users-service';
+import sendRequest from './send-request';
 
 const BASE_URL = '/api/items';
 
@@ -6,33 +6,6 @@ export function getAll() {
   return sendRequest(BASE_URL);
 }
 
-// This function is not used in SEI CAFE, however,
-// it's for demonstration purposes only
 export function getById(id) {
   return sendRequest(`${BASE_URL}/${id}`);
-}
-
-
-/*--- Helper Functions ---*/
-
-async function sendRequest(url, method = 'GET', payload = null) {
-  // Fetch accepts an options object as the 2nd argument
-  // used to include a data payload, set headers, etc. 
-  const options = { method };
-  if (payload) {
-    options.headers = { 'Content-Type': 'application/json' };
-    options.body = JSON.stringify(payload);
-  }
-  const token = getToken();
-  if (token) {
-    // Ensure that the headers objects exists
-    options.headers = options.headers || {};
-    // Add the Authorization header
-    // Prefacing the token with 'Bearer '
-    options.headers.Authorization = `Bearer ${token}`;
-  }
-  const res = await fetch(url, options);
-  // res.ok will be false if the status code set to 4xx in the controller action
-  if (res.ok) return res.json();
-  throw new Error('Bad Request');
 }
