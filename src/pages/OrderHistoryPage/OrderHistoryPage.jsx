b773dbd613a6cafe36dc4f455db3313b59bb911a
Jim Clark <jim.clark@generalassemb.ly>
2022-03-04 12:06:32 -0800

    Hackathon starter code

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,7 +1,20 @@
-export default function OrderHistoryPage() {
+import { Link } from 'react-router-dom';
+import './OrderHistoryPage.css';
+import Logo from '../../components/Logo/Logo';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
+
+export default function OrderHistoryPage({ user, setUser }) {
   return (
-    <>
-      <h1>OrderHistoryPage</h1>
-    </>
+    <main className="OrderHistoryPage">
+      <aside>
+        <Logo />
+        <Link to="/orders/new" className="button btn-sm">NEW ORDER</Link>
+        <UserLogOut user={user} setUser={setUser} />
+      </aside>
+      {/* Render an OrderList component (needs to be coded) */}
+
+      {/* Render the existing OrderDetail component */}
+
+    </main>
   );
 }
